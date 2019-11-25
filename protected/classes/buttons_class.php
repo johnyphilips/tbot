f1d68cb4dfbe4e21e03f50d220f1d05113f966ef
philips <johnyphilips@mail.ru>
2019-11-25 16:11:54 +0800

    Framework

--- protected/classes/buttons_class.php
+++ protected/classes/buttons_class.php
@@ -23,9 +23,11 @@ class buttons_class
                     ['text' => hex2bin('F09F8C90') . ' Deposit'],
                     ['text' => hex2bin('F09F8C90') . ' Withdraw Funds']
                 ],
+                [
                     ['text' => hex2bin('F09F8C90') . ' Referral Program'],
                     ['text' => hex2bin('F09F8C90') . ' About Us'],
                 ]
-            ];
+            ]
+        ];
     }
 }
