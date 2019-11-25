708e2ef71846ff9a9de5f4d77ae02624d3357911
philips <johnyphilips@mail.ru>
2019-11-25 16:52:53 +0800

    Framework

--- protected/templates/bot/en/deposit/deposit_info.php
+++ protected/templates/bot/en/deposit/deposit_info.php
@@ -1,6 +1,6 @@
 💰 You are going to deposit a total value of <code><?php echo round(number_format($sum, 8), 8); ?></code> BTC.
 
-You will purchase <b><?php echo $plan['plan_name']; ?> plan</b>.
+You will purchase <b><?php echo $plan['name']; ?> plan</b>.
 
 💡 <b>Reminder:</b> The address is active for <b><?php echo (bitcoin_service::WAIT_FOR_PAYMENT/60); ?> minutes</b>.
 
