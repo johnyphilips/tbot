5cd15f5aeddd2beaa2bd350a5276b3a5fbe50588
philips <johnyphilips@mail.ru>
2019-11-25 15:52:23 +0800

    Framework

--- protected/classes/buttons_class.php
+++ protected/classes/buttons_class.php
@@ -12,12 +12,16 @@ class buttons_class
         return [
             'ru' => [
                 [
-                    ['text' => hex2bin('F09F8C90') . ' TXT'],
+                    ['text' => hex2bin('F09F8C90') . ' '],
                 ]
             ],
             'en' => [
                 [
-                    ['text' => hex2bin('F09F8C90') . ' TXT'],
+                    ['text' => hex2bin('F09F8C90') . ' My Account (Balance 0$)'],
+                    ['text' => hex2bin('F09F8C90') . ' Deposit'],
+                    ['text' => hex2bin('F09F8C90') . ' Withdraw Funds'],
+                    ['text' => hex2bin('F09F8C90') . ' Referral Program'],
+                    ['text' => hex2bin('F09F8C90') . ' About Us'],
                 ]
             ]
         ];
