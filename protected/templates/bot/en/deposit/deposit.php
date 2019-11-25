280b99fa042f8b756c224be08dc1a201bbdcc9d1
philips <johnyphilips@mail.ru>
2019-11-25 18:43:34 +0800

    Framework

--- protected/templates/bot/en/deposit/deposit.php
+++ protected/templates/bot/en/deposit/deposit.php
@@ -1,1 +1,1 @@
-Enter sum to deposit from <b><?php echo $min_plan; ?> BTC</b> to <b><?php echo deposit_service::PLANS['professional']['to']; ?> BTC</b>
+Enter sum to deposit from <b><?php echo $min_sum; ?> BTC</b> to <b><?php echo deposit_service::PLANS['professional']['to']; ?> BTC</b>
