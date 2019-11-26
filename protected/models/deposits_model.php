17c10a9eed3dbf2f5ad37e360dccce274e1c22c7
philips <johnyphilips@mail.ru>
2019-11-26 17:03:12 +0800

    Framework

--- protected/models/deposits_model.php
+++ protected/models/deposits_model.php
@@ -9,12 +9,11 @@ class deposits_model extends model
 {
     public function getProfitDeposits()
     {
-        echo time() - (24/deposit_service::UPDATE_PROFIT_PER_DAY) * 3600;
         $stm = $this->pdo->prepare('
             SELECT
                 * 
             FROM
-                deposits WHERE last_profit <= ' . time() - (24/deposit_service::UPDATE_PROFIT_PER_DAY) * 3600 . '
+                deposits WHERE last_profit <= "' . time() - (24/deposit_service::UPDATE_PROFIT_PER_DAY) * 3600 . '"
         ');
         return $this->get_all($stm);
     }
