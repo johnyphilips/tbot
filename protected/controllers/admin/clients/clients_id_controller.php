2ee422d98b59fcd4ed009c2920974af36c5fc89d
philips <johnyphilips@mail.ru>
2019-11-26 20:56:51 +0800

    Framework

--- protected/controllers/admin/clients/clients_id_controller.php
+++ protected/controllers/admin/clients/clients_id_controller.php
@@ -13,36 +13,34 @@ class clients_id_controller extends admin_project
             $this->forbidden();
         }
         $user = $this->model('bot_users')->getById($_GET['id']);
+        $referrer = $this->model('bot_users')->getById($user['referrer_id']);
+        $this->render('referrer', $referrer);
         $this->render('user', $user);
-        $this->render('active_lotteries', $this->model('lotteries')->getUserActiveLotteries($user));
-        $this->render('closed_lotteries', $this->model('lotteries')->getUserClosedLotteries($user));
-        $this->render('payments', $this->model('payments')->getByField('user_id', $_GET['id'], true));
-        $this->render('withdrawals', $this->model('withdrawals')->getByField('user_id', $_GET['id'], true));
+        $this->render('deposits', $this->model('deposits')->getByField('user_id', $user['id'], true));
+        $this->render('withdrawals', $this->model('lotteries')->getByField('user_id', $user['id'], true));
+        $in = [];
+        $referrals = [];
+        foreach ($this->model('bot_users')->getByFields([
+            'referrer_id' => $user['id'],
+            'status_id' => bot_commands_class::USER_ACTIVE_STATUS
+        ], true) as $referral) {
+            $referrals[1][] = $referral;
+            $in[] = $referral['id'];
+        }
+        if($in) {
+            $in2 = [];
+            foreach ($this->model('bot_users')->getReferralsReferrals($in) as $referral) {
+                $referrals[2][] = $referral;
+                $in2[] = $referral['id'];
+            }
+            if($in2) {
+                foreach ($this->model('bot_users')->getReferralsReferrals($in2) as $referral) {
+                    $referrals[3][] = $referral;
+                }
+            }
+        }
+        $this->render('referrals', $referrals);
         $this->view('clients/id');
-    }
-
-    public function get_roulettes()
-    {
-        $params = [];
-        $params['table'] = 'roulettes r';
-        $params['select'] = [
-            'r.id',
-            'IF(r.status_id = ' . roulette_service::STATUS_ACTIVE . ', "Активная", "Завершена")',
-            'COUNT(b.id)',
-            'IF(r.status_id = ' . roulette_service::STATUS_ACTIVE . ', r.balance, won)',
-            'max_win',
-            'spent',
-            'r.create_date',
-            'r.close_date'
-        ];
-        $params['join']['roulette_bets'] = [
-            'as' => 'b',
-            'on' => 'b.roulette_id = r.id AND b.status_id = 3',
-            'left' => true
-        ];
-        $params['order'] = 'r.create_date DESC';
-        $params['group'] = 'r.id';
-        $this->success($this->module('data_table')->init($params, false, [6,7]));
     }
 
     protected function rules()
--- protected/templates/admin/en/clients/id.php
+++ protected/templates/admin/en/clients/id.php
@@ -33,12 +33,8 @@
                         </tr>
                     <?php endif; ?>
                     <tr>
-                        <td>Игровой баланс</td>
+                        <td>Баланс</td>
                         <td><?php echo $user['balance']; ?></td>
-                    </tr>
-                    <tr>
-                        <td>Призовой баланс</td>
-                        <td><?php echo $user['prize_balance']; ?></td>
                     </tr>
                     <tr>
                         <td>Дата</td>
@@ -49,26 +45,26 @@
         </div>
         <div class="card">
             <div class="card-header">
-                <h3 class="card-title">Пополнения</h3>
+                <h3 class="card-title">Депозиты</h3>
             </div>
             <div class="card-body">
                 <table class="table">
                     <thead>
                     <tr>
+                        <td>План</td>
                         <td>Сумма</td>
-                        <td>BTC</td>
                         <td>Статус</td>
                         <td>Дата</td>
                     </tr>
                     </thead>
                     <tbody>
-                    <?php if ($payments): ?>
-                        <?php foreach ($payments as $payment): ?>
+                    <?php if ($deposits): ?>
+                        <?php foreach ($deposits as $deposit): ?>
                             <tr>
-                                <td><?php echo $payment['amount']; ?></td>
-                                <td><?php echo $payment['amount_btc']; ?></td>
-                                <td><?php echo $payment['status_id']; ?></td>
-                                <td><?php echo $payment['create_date']; ?></td>
+                                <td><?php echo $deposit['plan']; ?></td>
+                                <td><?php echo $deposit['amount_btc']; ?></td>
+                                <td><?php echo $deposit['status_id']; ?></td>
+                                <td><?php echo $deposit['create_date']; ?></td>
                             </tr>
                         <?php endforeach; ?>
                     <?php endif; ?>
@@ -110,129 +106,9 @@
             </div>
         </div>
     </div>
-    <div class="col-md-6">
-        <div class="card">
-            <div class="card-header">
-                <h3 class="card-title">Активные лотереи</h3>
-            </div>
-            <div class="card-body">
-                <table class="table table-bordered">
-                    <tr>
-                        <td>Тираж</td>
-                        <td>Приз</td>
-                        <td>Тип</td>
-                        <td>Номера</td>
-                        <td></td>
-                    </tr>
-                    <?php foreach ($active_lotteries as $lottery): ?>
-                        <tr>
-                            <td>
-                                <?php echo $lottery['edition_id']; ?>
-                            </td>
-                            <td>
-                                <?php echo $lottery['amount']; ?>
-                            </td>
-                            <td>
-                                <?php echo $lottery['total_bets']; ?> из <?php echo $lottery['cells_qty']; ?> за <?php echo $lottery['cell_price']; ?>
-                            </td>
-                            <td>
-                                <?php echo implode(',', $lottery['bets']); ?>
-                            </td>
-                            <td>
-                                <a href="/editions/id?id=<?php echo $lottery['edition_id']; ?>"><i class="fas fa-link"></i> </a>
-                            </td>
-                        </tr>
-                    <?php endforeach; ?>
-                </table>
-            </div>
-        </div>
-        <div class="card">
-            <div class="card-header">
-                <h3 class="card-title">Старые лотереи</h3>
-            </div>
-            <div class="card-body">
-                <table class="table table-bordered">
-                    <tr>
-                        <td>Тираж</td>
-                        <td>Приз</td>
-                        <td>Цена</td>
-                        <td>Номера</td>
-                        <td>Победитель</td>
-                        <td></td>
-                    </tr>
-                    <?php foreach ($closed_lotteries as $lottery): ?>
-                        <tr>
-                            <td>
-                                <?php echo $lottery['edition_id']; ?>
-                            </td>
-                            <td>
-                                <?php echo $lottery['amount']; ?>
-                            </td>
-                            <td>
-                                <?php echo $lottery['cells_qty']; ?> по <?php echo $lottery['cell_price']; ?>
-                            </td>
-                            <td>
-                                <?php echo implode(',', $lottery['bets']); ?>
-                            </td>
-                            <td>
-                                <a href="/cliens/id?id=<?php echo $lottery['winner_id']; ?>">@<?php echo $lottery['t_user_name']; ?></a>
-                            </td>
-                            <td>
-                                <a href="/editions/id?id=<?php echo $lottery['edition_id']; ?>"><i class="fas fa-link"></i> </a>
-                            </td>
-                        </tr>
-                    <?php endforeach; ?>
-                </table>
-            </div>
-        </div>
-    </div>
-</div>
-<div class="row clearfix">
-    <div class="col-md-12">
-        <div class="card">
-            <div class="card-header">
-                <h3 class="card-title">Рулетки</h3>
-            </div>
-            <div class="card-body custom-datatable">
-                <table class="table table-bordered" id="get_roulettes">
-                    <thead>
-                    <tr>
-                        <td>
-                            <input type="text" class="form-control filter-field" name="r.id" data-sign="=" autocomplete="off">
-                            <input type="hidden" class="filter-field" name="r.user_id" data-sign="=" value="<?php echo $_GET['id']; ?>">
-                        </td>
-                        <td>
-                            <select class="form-control filter-field" name="r.id" data-sign="=">
-                                <option value=""></option>
-                                <option value="<?php echo roulette_service::STATUS_ACTIVE; ?>">Активная</option>
-                                <option value="<?php echo roulette_service::STATUS_CLOSED; ?>">Закончена</option>
 
-                            </select>
-                        </td>
-                        <td></td>
-                        <td><input type="text" class="form-control filter-field" name="r.won" data-sign="=" autocomplete="off"> </td>
-                        <td> </td>
-                        <td><input type="text" class="form-control filter-field" name="r.spent" data-sign="=" autocomplete="off"> </td>
-                        <td><input type="text" class="form-control filter-field dp" name="DATE(r.create_date)" data-sign="=" autocomplete="off"> </td>
-                        <td><input type="text" class="form-control filter-field dp" name="DATE(r.close_date)" data-sign="=" autocomplete="off"> </td>
-                    </tr>
-                    <tr>
-                        <th>ID</th>
-                        <th>Статус</th>
-                        <th>Спинов</th>
-                        <th>Выигрыш</th>
-                        <th>Макс</th>
-                        <th>Потрачено</th>
-                        <th>Дата начала</th>
-                        <th>Дата завершения</th>
-                    </tr>
-                    </thead>
-                    <tbody></tbody>
-                </table>
-            </div>
-        </div>
-    </div>
 </div>
+
 <div class="modal fade" id="message_modal">
     <div class="modal-dialog">
         <div class="modal-content">
