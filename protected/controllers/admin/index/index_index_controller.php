fdab0934a257cc1ab8165d8f360d0eaa82c0a8f2
philips <johnyphilips@mail.ru>
2019-11-26 19:46:56 +0800

    Framework

--- protected/controllers/admin/index/index_index_controller.php
+++ protected/controllers/admin/index/index_index_controller.php
@@ -53,7 +53,7 @@ class index_index_controller extends index_helper
         }
         foreach ($this->model('bot_users')->count30DaysBlockedUsers() as $date => $item) {
             if($stats[$date]) {
-                $stats[$date]['new_users'] = $item;
+                $stats[$date]['blocked_users'] = $item;
             }
         }
 //        foreach ($this->model('lotteries')->count30DaysLotteries() as $date => $item) {
