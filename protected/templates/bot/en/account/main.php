2257581d05e184615cac6b568a852320fadca746
philips <johnyphilips@mail.ru>
2019-11-26 16:28:39 +0800

    Framework

--- protected/templates/bot/en/account/main.php
+++ protected/templates/bot/en/account/main.php
@@ -6,11 +6,12 @@ Account Menu
 <?php if ($deposits): ?>
 <b>Your deposits</b>
 <?php foreach ($deposits as $plan => $plan_deposits): ?>
-<?php echo ucfirst($plan); ?>
+<b><?php echo ucfirst($plan); ?></b>
 <?php foreach ($plan_deposits as $deposit): ?>
 Deposit: <?php echo bitcoin_service::formatBTC($deposit['amount_btc']); ?> BTC
 Profit: <?php echo bitcoin_service::formatBTC($deposit['profit']); ?> BTC
-End Date: <?php echo (deposit_service::PLANS[$deposit['plan']]['term'] - tools_class::dateDiff($deposit['create_date'])->days); ?>
+End in <?php echo (deposit_service::PLANS[$deposit['plan']]['term'] - tools_class::dateDiff($deposit['create_date'])->days); ?> days
+
 <?php endforeach; ?>
 <?php endforeach; ?>
 <?php endif; ?>
