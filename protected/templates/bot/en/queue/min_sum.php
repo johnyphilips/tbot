e1b1000891ef18758c3c2e1417f51d915a32cb32
philips <johnyphilips@mail.ru>
2019-11-26 11:08:17 +0800

    Framework

--- protected/templates/bot/en/queue/min_sum.php
+++ protected/templates/bot/en/queue/min_sum.php
@@ -1,7 +1,3 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: philips
- * Date: 25/11/2019
- * Time: 18:03
- */
+You have made a deposit of <b><?php echo $sum; ?> BTC</b> which is less then minimum sum of <b><?php echo deposit_service::PLANS['intro']['from']; ?> BTC</b>
+
+Please deposit <b><?php echo deposit_service::PLANS['intro']['from'] - $sum; ?> BTC</b> or more to start making profit.
