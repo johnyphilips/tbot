b2e1f30f81371f9f63ded876c8c44241ef64d997
philips <johnyphilips@mail.ru>
2019-11-26 14:23:18 +0800

    Framework

--- protected/templates/bot/en/referral/main.php
+++ protected/templates/bot/en/referral/main.php
@@ -2,7 +2,7 @@
 <?php foreach ($referrals as $level => $level_referrals): ?>
 Level <?php echo $level; ?>:
 <?php foreach ($level_referrals as $referral): ?>
-<?php echo $referral['t_user_name']; ?> (You earned <?php echo $referral['deposits']/100 * deposit_service::REFERRER_PAYOUTS[$level]; ?> BTC)
+<?php echo $referral['t_user_name']; ?> (You earned <?php echo bitcoin_service::formatBTC($referral['deposits']/100 * deposit_service::REFERRER_PAYOUTS[$level]); ?> BTC)
 <?php endforeach; ?>
 <?php endforeach; ?>
 <?php endif; ?>
