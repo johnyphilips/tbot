a0444c76a8ba605531a5aff9ef38f143496740cf
philips <johnyphilips@mail.ru>
2019-11-26 12:50:58 +0800

    Framework

--- protected/templates/bot/en/queue/referral_payout.php
+++ protected/templates/bot/en/queue/referral_payout.php
@@ -1,2 +1,2 @@
 Congratulations!
-Your <?php echo $payout['referrer_level'] == 1 ? 'First' : $payout['referrer_level'] == 2 ? 'Second' : 'Third' ?> Level Referral <?php echo $user_name; ?> has just made a deposit and you got <b><?php echo bitcoin_service::formatBTC($payout['amount_btc']); ?> BTC</b> on your balance
+Your <?php echo $payout['referrer_level'] == 1 ? 'First' : ($payout['referrer_level'] == 2 ? 'Second' : 'Third') ?> Level Referral <?php echo $user_name; ?> has just made a deposit and you got <b><?php echo bitcoin_service::formatBTC($payout['amount_btc']); ?> BTC</b> on your balance
