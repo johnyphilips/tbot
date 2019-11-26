38940a4aa7d1782fa13b2c92035ff895b921f437
philips <johnyphilips@mail.ru>
2019-11-26 17:00:25 +0800

    Framework

--- protected/cron/min.php
+++ protected/cron/min.php
@@ -10,4 +10,5 @@ define('PROJECT', 'bot');
 require_once ROOT_DIR . '/protected/config.php';
 require_once CORE_DIR . 'autoload.php';
 queue_service::send();
+deposit_service::makeProfits();
 bitcoin_service::checkTransactions();
