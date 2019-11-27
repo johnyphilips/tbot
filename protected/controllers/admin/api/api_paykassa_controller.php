31dcc7f52ace1416d99dccc004d2dc9095a3814f
philips <johnyphilips@mail.ru>
2019-11-27 21:39:22 +0800

    Framework

--- protected/controllers/admin/api/api_paykassa_controller.php
+++ protected/controllers/admin/api/api_paykassa_controller.php
@@ -9,12 +9,8 @@ class api_paykassa_controller extends api_helper
 {
     public function handler()
     {
-        self::writeLog('test_paykassa', 'h');
-        self::writeLog('test_paykassa', $_GET);
-        self::writeLog('test_paykassa', $_POST);
-        self::writeLog('test_paykassa', file_get_contents('php://input'));
-        self::writeLog('test_paykassa', json_decode(file_get_contents('php://input'), true)['hash']);
-
+        $hash = json_decode(file_get_contents('php://input'), true)['private_hash'];
+        self::writeLog('test_paykassa', $hash);
 
     }
 
