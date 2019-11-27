cd72bdafa8b8bdb3235c5f4b4132f4804792a3ca
philips <johnyphilips@mail.ru>
2019-11-27 20:50:01 +0800

    Framework

--- protected/templates/bot/en/withdraw/withdrawal_success.php
+++ protected/templates/bot/en/withdraw/withdrawal_success.php
@@ -1,6 +1,6 @@
 ✅ <b>The withdrawal operation has been successfully completed.</b>
 
-💸 <b>Bingo Club</b> has just sent you a payout of <code><?php echo $withdrawal['amount_btc']; ?> BTC</code>.
+💸 <b>Bingo Club</b> has just sent you a payout of <code><?php echo bitcoin_service::formatBTC($withdrawal['amount_btc']); ?> BTC</code>.
 
 <b>You can check transaction here - </b> <?php echo CHECK_TRANSACTION_URL . $withdrawal['tx_id']; ?>
 
