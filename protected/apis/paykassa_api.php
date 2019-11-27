c4f1de7f0babbb4967f906d88c024e14c555ddf2
philips <johnyphilips@mail.ru>
2019-11-27 22:25:35 +0800

    Framework

--- protected/apis/paykassa_api.php
+++ protected/apis/paykassa_api.php
@@ -54,9 +54,9 @@ class paykassa_api extends staticApi
         ];
         $res = self::sendRequest('sci_confirm_order', $params, true);
         self::writeLog('test_req', $res);
-        self::writeLog('test_req', ['data']['error']);
-        self::writeLog('test_req', ['data']['amount']);
-        if($res['data']['error'] === false && $res['data']['amount']) {
+        self::writeLog('test_req', $res['error']);
+        self::writeLog('test_req', $res['data']['amount']);
+        if($res['error'] === false && $res['data']['amount']) {
             return $res['data']['amount'];
         }
         return false;
