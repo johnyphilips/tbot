9128431cba52ec4bca07ddca9bedd4d8d558aeb0
philips <johnyphilips@mail.ru>
2019-11-28 16:29:11 +0800

    Framework

--- protected/templates/bot/en/referral/main.php
+++ protected/templates/bot/en/referral/main.php
@@ -5,4 +5,6 @@ Level <?php echo $level; ?>:
 <?php echo $referral['t_user_name']; ?> (You earned <?php echo bitcoin_service::formatBTC($referral['deposits']/100 * deposit_service::REFERRER_PAYOUTS[$level]); ?> BTC)
 <?php endforeach; ?>
 <?php endforeach; ?>
+<?php else: ?>
+You don't have referrals
 <?php endif; ?>
