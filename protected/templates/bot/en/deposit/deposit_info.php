343d0d15882c59ecda0511bd44bd9085a38e3c9a
philips <johnyphilips@mail.ru>
2019-11-28 14:27:48 +0800

    Framework

--- protected/templates/bot/en/deposit/deposit_info.php
+++ protected/templates/bot/en/deposit/deposit_info.php
@@ -1,9 +1,7 @@
-💰 You are going to deposit a total value of <code><?php echo bitcoin_service::formatBTC($sum); ?></code> BTC.
+💎 Great choice.
+✅ Your plan: <b><?php echo strtoupper($plan['name']); ?> plan</b>
 
-You will purchase <b><?php echo $plan['name']; ?> plan</b>.
+💡 Your investment plan will be activated as soon as we receive 3 confirmation on the Bitcoin network.
+You will receive a notification that your plan is activated.  And you will be credited with profit every 3 hours.
 
-💡 <b>Reminder:</b> The address is active for <b><?php echo (bitcoin_service::WAIT_FOR_PAYMENT/60); ?> minutes</b>.
-
-♻️ <b>After this time has expired, please request a new BTC address.</b>
-
-👇 To complete the deposit transfer <code><?php echo bitcoin_service::formatBTC($sum); ?></code> BTC to the address below.
+👇🏽 To open the plan, transfer <code><?php echo bitcoin_service::formatBTC($sum); ?></code> BTC to the following BTC address:
