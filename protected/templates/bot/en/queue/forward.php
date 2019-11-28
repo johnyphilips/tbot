eae1727775a2adaf900ccdfbdbac51eec40ac6df
philips <johnyphilips@mail.ru>
2019-11-28 16:53:31 +0800

    Framework

---
+++ protected/templates/bot/en/queue/forward.php
@@ -0,0 +1,3 @@
+You can get <code><?php echo $sum; ?> BTC</code> on your balance <b>for free!</b>
+
+Just forward previous message with withdrawal information to our chat <?php echo CHAT_NAME; ?> ?>
