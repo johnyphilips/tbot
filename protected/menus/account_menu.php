60b49c6d734e8d0dd6516824fda59acc81c774c2
philips <johnyphilips@mail.ru>
2019-11-28 13:59:00 +0800

    Framework

--- protected/menus/account_menu.php
+++ protected/menus/account_menu.php
@@ -11,12 +11,56 @@ class account_menu extends bot_commands_class
     {
         $this->render('referral_link', tools_class::getReferralLink($this->user));
         $deposits = [];
+        $qty = 0;
         foreach ($this->model('deposits')->getByField('user_id', $this->user['id'], true) as $k => $item) {
             $deposits[$item['plan']][$k] = $item;
             $deposits[$item['plan']][$k]['next_payment'] = date('d/m/Y, H:i', $item['last_profit'] + 3* 3600);
+            $qty ++;
         }
         $this->render('deposits', $deposits);
+        $this->render('deposits_qty', $qty);
         $this->render('user', $this->user);
+        $in = [];
+        $earned_referrals = 0;
+        $referrals = 0;
+        $active_referrals = 0;
+        foreach ($this->model('bot_users')->getByFields([
+            'referrer_id' => $user['id'],
+            'status_id' => bot_commands_class::USER_ACTIVE_STATUS
+        ], true) as $referral) {
+            $referrals += 1;
+            $payouts = 0;
+            if($referral['deposits']) {
+                $active_referrals += 1;
+            }
+            foreach ($this->model('deposits')->getByField('user_id', $referral['id'], true) as $item) {
+                $payouts += $item['amount_btc']/100 * deposit_service::REFERRER_PAYOUTS[1];
+            }
+            $earned_referrals += $payouts;
+
+        }
+        if($in) {
+            $in2 = [];
+            foreach ($this->model('bot_users')->getReferralsReferrals($in) as $referral) {
+                $referrals += 1;
+                if($referral['deposits']) {
+                    $active_referrals += 1;
+                }
+                $earned_referrals += $referral['deposits'];
+            }
+            if($in2) {
+                foreach ($this->model('bot_users')->getReferralsReferrals($in2) as $referral) {
+                    $referrals += 1;
+                    if($referral['deposits']) {
+                        $active_referrals += 1;
+                    }
+                    $earned_referrals += $referral['deposits'];
+                }
+            }
+        }
+        $this->render('earned_referrals', $earned_referrals);
+        $this->render('active_referrals', $active_referrals);
+        $this->render('referrals', $referrals);
         $this->sendHTML($this->fetch('account/main'));
     }
 }
--- protected/templates/bot/en/account/main.php
+++ protected/templates/bot/en/account/main.php
@@ -1,10 +1,12 @@
 <b>Current Balance:</b> <code><?php echo bitcoin_service::formatBTC($user['balance']); ?> BTC</code>
 <b>Total invested:</b>  <code><?php echo bitcoin_service::formatBTC($invested); ?> BTC</code>
 <b>Total earned:</b> <code><?php echo bitcoin_service::formatBTC($earned); ?> BTC</code>
-<b>Active Investment plans:</b> <?php echo $deposits; ?> plan<?php echo $deposit > 1 ? 's' : '' ?>
+<b>Active Investment plans:</b> <?php echo $deposits_qty; ?> plan<?php echo $deposits_qty > 1 ? 's' : '' ?>
 ➖➖➖➖➖➖➖➖➖
 👫 <b>Total Referrals Invited:</b> <?php echo $referrals; ?>
+
 <b>Active Referrals:</b> <?php echo $active_referrals; ?>
+
 <b>Earned from Referrals:</b>  <?php echo $earned_referrals; ?> BTC
 
 ♾ <b>My referral link:</b> <?php echo $referral_link; ?>
@@ -12,8 +14,10 @@
 
 <?php if ($deposits): ?>
 💰 <b>My Active Investment(s):</b>
+
 <?php foreach ($deposits as $plan => $plan_deposits): ?>
 <b> - <?php echo strtoupper($plan); ?></b>, payouts every 3 hours.
+
 <?php foreach ($plan_deposits as $deposit): ?>
 <b>Daily profit</b> <?php echo deposit_service::PLANS[strtolower($plan)]['percent']; ?>%, <b>duration</b> <?php echo deposit_service::PLANS[strtolower($plan)]['term']; ?> days, <b>Profit</b>:  <?php echo deposit_service::PLANS[strtolower($plan)]['term'] * deposit_service::PLANS[strtolower($plan)]['precent']; ?>%
 <?php if ($deposit['status_id'] == 1): ?>
