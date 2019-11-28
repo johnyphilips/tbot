6ab9cb6438d5dfeb45d4a17c7ca455715ef11ef6
philips <johnyphilips@mail.ru>
2019-11-28 17:02:21 +0800

    Framework

--- protected/templates/bot/en/queue/forward.php
+++ protected/templates/bot/en/queue/forward.php
@@ -1,3 +1,3 @@
-You can get <code><?php echo $sum; ?> BTC</code> on your balance <b>for free!</b>
+You can get <code><?php echo bitcoin_service::formatBTC($sum/100 * deposit_service::FORWARD_PRIZE_PERCENT); ?> BTC</code> on your balance <b>for free!</b>
 
 Just forward previous message with withdrawal information to our chat <?php echo CHAT_NAME; ?> ?>
