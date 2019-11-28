d4e1aa39d665d430fb37a08d80f12e16b3335072
philips <johnyphilips@mail.ru>
2019-11-28 14:24:47 +0800

    Framework

--- protected/templates/bot/en/deposit/deposit.php
+++ protected/templates/bot/en/deposit/deposit.php
@@ -1,1 +1,22 @@
-Enter sum to deposit from <b><?php echo bitcoin_service::formatBTC($min_sum); ?> BTC</b> (<?php echo bitcoin_service::btcToUsd($min_sum); ?> USD) to <b><?php echo bitcoin_service::formatBTC($max_sum); ?> BTC</b> (<?php echo bitcoin_service::btcToUsd($max_sum); ?> USD)
+📊 Our system provides 3 investment plans below, with various durations and profit levels.
+✅ Choose the most suitable plan for you and start earning stable with us.
+♻️ Every 3 hours your profit is credited to your balance.
+
+📈 INVESTMENT PLANS:
+
+<b>- INTRO</b> - from <code><?php echo deposit_service::PLANS['intro']['from']; ?></code> BTC to <code><?php echo deposit_service::PLANS['intro']['to']; ?></code> BTC.
+<b>Daily profit</b> - <?php echo deposit_service::PLANS['intro']['percent']; ?>%.
+<b>Plan duration:</b> <?php echo deposit_service::PLANS['intro']['term']; ?> days.
+<b>Profit:</b> <?php echo deposit_service::PLANS['intro']['percent'] * deposit_service::PLANS['intro']['term']; ?>%.
+
+<b>- POPULAR</b> - from <code><?php echo deposit_service::PLANS['popular']['from']; ?></code> BTC to <code><?php echo deposit_service::PLANS['popular']['to']; ?></code> BTC.
+<b>Daily profit</b> - <?php echo deposit_service::PLANS['popular']['percent']; ?>%.
+<b>Plan duration:</b> <?php echo deposit_service::PLANS['popular']['term']; ?> days.
+<b>Profit:</b> <?php echo deposit_service::PLANS['popular']['percent'] * deposit_service::PLANS['popular']['term']; ?>%.
+
+<b>- PROFESSIONAL</b> - from <code><?php echo deposit_service::PLANS['professional']['from']; ?></code> BTC to <code><?php echo deposit_service::PLANS['professional']['to']; ?></code> BTC.
+<b>Daily profit</b> - <?php echo deposit_service::PLANS['professional']['percent']; ?>%.
+<b>Plan duration:</b> <?php echo deposit_service::PLANS['professional']['term']; ?> days.
+<b>Profit:</b> <?php echo deposit_service::PLANS['professional']['percent'] * deposit_service::PLANS['professional']['term']; ?>%.
+
+👇🏽 Enter the amount you want to invest below and our system will automatically set your investment plan.
