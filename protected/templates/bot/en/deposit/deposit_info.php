8c0f2064be4ec0604a456087b5e5e2e9b8a2f1bd
philips <johnyphilips@mail.ru>
2019-11-28 14:29:06 +0800

    Framework

--- protected/templates/bot/en/deposit/deposit_info.php
+++ protected/templates/bot/en/deposit/deposit_info.php
@@ -2,6 +2,6 @@
 ✅ Your plan: <b><?php echo strtoupper($plan['name']); ?></b>
 
 💡 Your investment plan will be activated as soon as we receive 3 confirmation on the Bitcoin network.
-You will receive a notification that your plan is activated.  And you will be credited with profit every 3 hours.
+You will receive a notification that your plan is activated.  And you will be credited with <b>profit every 3 hours</b>.
 
 👇🏽 To open the plan, transfer <code><?php echo bitcoin_service::formatBTC($sum); ?></code> BTC to the following BTC address:
