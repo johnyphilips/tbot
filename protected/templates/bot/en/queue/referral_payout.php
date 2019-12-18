7bad68fe4a85774a55452b0d5b5701a593cfd031
philips <johnyphilips@mail.ru>
2019-12-18 17:16:31 +0700

    Framework

--- protected/templates/bot/en/queue/referral_payout.php
+++ protected/templates/bot/en/queue/referral_payout.php
@@ -1,4 +1,4 @@
-smile_F09F8F86 A referral from your <b>Level <?php echo $payout['referrer_level']; ?></b> has just made an investment, your profit of <b><?php echo bitcoin_service::formatBTC($payout['amount_btc']); ?> BTC</b> (<?php echo deposit_service::REFERRER_PAYOUTS[$payout['referral_level']]; ?>%) has been transferred to your balance.
+smile_F09F8F86 A referral from your <b>Level <?php echo $payout['referrer_level']; ?></b> has just made an investment, your profit of <b><?php echo bitcoin_service::formatBTC($payout['amount_btc']); ?> BTC</b> (<?php echo deposit_service::REFERRER_PAYOUTS[$payout['referrer_level']]; ?>%) has been transferred to your balance.
 
 smile_F09F9A80 <b>Congratulations!</b>
 
