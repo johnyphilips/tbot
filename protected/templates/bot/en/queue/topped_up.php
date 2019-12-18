8b7f030ae2af746ad460572440a8568a053e0e98
philips <johnyphilips@mail.ru>
2019-12-18 16:18:03 +0700

    Framework

--- protected/templates/bot/en/queue/topped_up.php
+++ protected/templates/bot/en/queue/topped_up.php
@@ -1,3 +1,3 @@
 smile_E29C85 Your investment Plan <b><?php echo $plan['name']; ?></b> has been successfully activated.
-smile_F09F92B5 You will receive your first profit on <?php echo date('d/m/Y, H:i', strtotime(date('Y-m-d H:i:s') . ' + ' . 24/deposit_service::UPDATE_PROFIT_PER_DAY . ' + hour')); ?>,(UTC ).
+smile_F09F92B5 You will receive your first profit on <?php echo date('d/m/Y, H:i', strtotime(date('Y-m-d H:i:s') . ' + ' . 24/deposit_service::UPDATE_PROFIT_PER_DAY . ' + hours')); ?>,(UTC ).
 <b>Great earnings.</b>
